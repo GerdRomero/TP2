23af96e9798a5dd02409a6dace489fee98672658
Gerard Romero <33358269+GerdRomero@users.noreply.github.com>
2018-05-23 14:42:39 -0300

    sembrar, regar cosechar

--- Terreno.cpp
+++ Terreno.cpp
@@ -8,14 +8,17 @@
 #include "Terreno.h"
 
 Terreno::Terreno() {
-	this->estaSembrada=false;
+	this->Sembrada=false;
 	this->parcelaPodrida=false;
-	this->terreno=0;
+	this->terreno = NULL;
 	this->seCosecho=false;
-	this->estado=VACIA;
-	this->rentabilidad=0;
-	this->turnosDeRecuperacion=0;
-	this->turnosParaCosechar=0;
+	this->Parcela.tipo = '#';
+	this->Parcela.numeroDeRiegos = 0;
+	this->Parcela.recuperacion = 0;
+	this->Parcela.rentabilidad = 0;
+	this->Parcela.tiempoHastaCosecha = 0;
+	this->Parcela.tiempoTrasSiembra = 0;
+	this->ESTADO = vacia;
 
 }
 
@@ -26,5 +29,75 @@ Terreno** Terreno::crearTerreno(ui fila, ui columna){
 
 	return terreno;
 }
+
+bool Terreno::estaSembrada(){
+	return(!((this->ESTADO == vacia) && !(this->parcelaPodrida)));
+}
+
+bool Terreno::regoCorrectamente(){
+	return (this->Parcela.numeroDeRiegos == this->Parcela.tiempoHastaCosecha);
+}
+
+void Terreno::cambiarACosechado(){
+	this->Parcela.rentabilidad  = 0;
+	this->Parcela.tiempoHastaCosecha = 0;
+	this->ESTADO = reposo;
+	this->Parcela.numeroDeRiegos = 0;
+	this->seCosecho = true;
+	this->Parcela.tipo = '#';
+}
+
+void Terreno::recuperarParcela(){
+	if ((this->seCosecho && (this->Parcela.recuperacion == 0)) ||
+			(this->parcelaPodrida && (this->Parcela.recuperacion == 0))){
+		this->Sembrada = false;
+	}
+}
+
+void Terreno::decrementarRecuperacion(){
+	if (((this->Parcela.recuperacion > 0) && this->seCosecho) || (this->parcelaPodrida  && this->Parcela.recuperacion > 0)){
+		this->Parcela.recuperacion --;
+	}
+}
+
+void Terreno::incrementarTiempoTrasSiembra(){
+	this->Parcela.tiempoTrasSiembra++;
+}
+
+void Terreno::podrirParcela(){
+	if((this->Parcela.numeroDeRiegos) < (this->Parcela.tiempoTrasSiembra)){
+		this->ESTADO = reposo;
+		this->Parcela.tipo = 'P';
+		this->Parcela.rentabilidad  = 0;
+		this->Parcela.tiempoHastaCosecha = 0;
+		this->Parcela.numeroDeRiegos = 0;
+		this->parcelaPodrida = true;
+	}
+}
+
+void Terreno::actualizando(){
+	this->incrementarTiempoTrasSiembra();
+	this->podrirParcela();
+	this->decrementarRecuperacion();
+	this->recuperarParcela();
+}
+
+ui Terreno::obtenerRentabilidad(){
+	return this->Parcela.rentabilidad;
+}
+
+void Terreno::aumentarNumeroDeRiegos(){
+	this->Parcela.numeroDeRiegos++;
+}
+
+void Terreno::cambiarEstadoDeParcelaSembrada(Semillas * semilla){
+	this->Parcela.rentabilidad = semilla->obtenerRentabilidad();
+	this->Parcela.recuperacion = semilla->obtenerRecuperacion();
+	this->Parcela.tiempoHastaCosecha = semilla->obtenertiempoHastaCosecha();
+	this->Sembrada = true;
+	this->ESTADO = sembrada;
+	this->Parcela.numeroDeRiegos = 0;
+}
+
 
 
