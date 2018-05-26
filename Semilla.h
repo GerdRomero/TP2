f0600734c803396ddd05702a62d7c9d7e817185e
Gerard Romero <33358269+GerdRomero@users.noreply.github.com>
2018-05-26 20:29:11 -0300

    comentarios

--- Semilla.h
+++ Semilla.h
@@ -23,15 +23,30 @@ private:
 	char tipo;
 public:
 	Semilla(Lista<std::string> &datos);
-	char obtenerTipo(){
-		return this->tipo;
-	}
+	//void agregarInfo(Lista<std::string>*datos);
 	ui precio();
+	/* post: Agrego el costo de la Semilla*/
+	void agregarCosto(std::string costo);
+	/* post: Agrego el tiempo de Cosecha de la Semilla*/
+	void agregarTiempoCosec(std::string turnos);
+	/* post: Agrego la rentabilidad de la Semilla*/
+	void agregarRentabilidad(std::string ganancia);
+	/* post: Agrego el tiempo de recuperacion de la Semilla*/
+	void agregarTiempoRecup(std::string recuperacion);
+	/* post: Agrego la cantidaqd de agua que requiere la Semilla*/
+	void agregarAguaTurno(std::string aguaPorTurno);
+	/* post: Devuelve la cantidad de semillas compradas*/
+	ui cantDisponible()
+	/* post: Devuelve la cantidad de agua que requiere la Semilla*/
 	ui cantidadAguaTurno();
+	/* post: Devuelve el tiempo de recuperacion*/
 	ui turnosRecuperacion();
+	/* post: Devuelve la rentabilidad */
 	ui ganancia();
+	/* post: Devuelve los turnos hasta cosechar*/
 	ui turnosCosecha();
 	void agregar(ui cantAgregar);
+	/* post: Devuelve true si hay semillas comrpadas. */
 	bool haySemilla();
 	virtual ~Semilla();
 };
